4d7a5faf8ec41958e010800a6e00730276725d07
unknown <2511500015@mahasiswa.atmaluhur.ac.id>
2025-10-28 14:49:22 +0700

    update script.js pertemuan-05

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -12,7 +12,14 @@ document.querySelector("form").addEventListener("submit", function (e) {
 });
 
 document.getElementById("menuToggle").addEventListener("click", function () {
-    document.querySelector("nav").classList.toggle("active");
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("active");
+
+    if (nav.classList.contains("active")) {
+        this.textContent = "\u2716"
+    } else {
+        this.textContent = "\u2630"
+    }
 });
 
 document.querySelector("form").addEventListener("submit", function (e) {
@@ -106,3 +113,4 @@ window.addEventListener("resize", () => {
         if (target) alignErrorMessage(small, target);
     });
 });
+
