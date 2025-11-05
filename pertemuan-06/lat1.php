475c6647a063bcb0a08febf2cb6cd626e20f95bc
Abraar2511500015 <2511500015@mahasiswa.atmaluhur.ac.id>
2025-11-05 23:36:17 +0700

    style.css pertemuan-06

--- pertemuan-06/lat1.php
+++ pertemuan-06/lat1.php
@@ -7,3 +7,49 @@
 <h1><?php echo "Halo, Dunia PHP!"; ?></h1>
 </body>
 </html>
+<?php
+$nama = "Moh. Sayyid Abraar";
+$umur = "18";
+$tinggi = "1.60";
+$aktif = "true";
+
+echo "Nama: $nama <br>";
+echo "Umur: $umur tahun <br>";
+echo "Tinggi: $tinggi meter <br>";
+echo "Status aktif: " . ($aktif ? "Ya" : "Tidak") . "<br>";
+var_dump($nama);
+var_dump($umur);
+var_dump($tinggi);
+var_dump($aktif);
+?>
+
+<?php
+$nama = "Moh. Sayyid Abraar";
+$umur = 18;
+$tinggi = 1.60;
+$aktif = true;
+$hobi = ["Bermain Game", "Membaca Buku"];
+$mahasiswa = (object)[
+"nim" => "2511500015",
+"nama" => "Moh. Sayyid Abraar",
+"prodi" => "Teknik Informatika"
+];
+$nilai_akhir = NULL;
+echo "<h2>Demo Tipe Data PHP</h2>";
+echo "<pre>";
+echo "String:\n";
+var_dump($nama);
+echo "\nInteger:\n";
+var_dump($umur);
+echo "\nFloat:\n";
+var_dump($tinggi);
+echo "\nBoolean:\n";
+var_dump($aktif);
+echo "\nArray:\n";
+var_dump($hobi);
+echo "\nObject:\n";
+var_dump($mahasiswa);
+echo "\nNULL:\n";
+var_dump($nilai_akhir);
+echo "</pre>";
+?>
