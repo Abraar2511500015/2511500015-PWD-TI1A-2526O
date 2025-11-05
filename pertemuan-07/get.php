bc0ca2204b6541755c359c735e5e0c93334ff6dd
Abraar2511500015 <2511500015@mahasiswa.atmaluhur.ac.id>
2025-11-05 12:44:31 +0700

    get.php pertemuan-07

--- pertemuan-07/get.php
+++ pertemuan-07/get.php
@@ -1,5 +1,8 @@
 <?php
   session_start();
+  if (isset($_SESSION["nama"])):
+    $sesname = $_SESSION["nama"];
+  endif; 
   $sesname = $_SESSION["nama"];
   $sesemail = $_SESSION["email"];
   $sespesan = $_SESSION["pesan"];
