c7c8b731da30662daff53385dd41b20d3a0b8771
Abraar2511500015 <2511500015@mahasiswa.atmaluhur.ac.id>
2025-11-06 18:47:16 +0700

    index.php pertemuan-06

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -159,7 +159,31 @@
                 else return "E";
             }
 
-            
+            function hitungMutu($grade): float
+            {
+                switch ($grade) {
+                    case "A":
+                        return 4.0;
+                    case "A-":
+                        return 3.7;
+                    case "B+":
+                        return 3.3;
+                    case "B":
+                        return 3.0;
+                    case "B-":
+                        return 2.7;
+                    case "C+":
+                        return 2.3;
+                    case "C":
+                        return 2.0;
+                    case "C-":
+                        return 1.7;
+                    case "D":
+                        return 1.0;
+                    default:
+                        return 0.0;
+                }
+            }
         <section id="contact">
             <h2>Kontak Saya</h2>
             <form action="" method="GET">
