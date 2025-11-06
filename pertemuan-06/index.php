c3caf5d50eac95bfd2c3e2ac0bb1116bb4bce342
Abraar2511500015 <2511500015@mahasiswa.atmaluhur.ac.id>
2025-11-06 18:40:54 +0700

    index.php pertemuan-06

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -48,55 +48,118 @@
             <h2>Tentang Saya</h2>
                 <p><strong>NIM:</strong>
                     <?php
-                        echo $nim;
+                    echo $nim;
                     ?>
                 </p>
                 <p><strong>Nama:</strong>
                     <?php
-                        echo $Nama
+                    echo $Nama
                     ?>
                 </p>
                 <p><strong>Tempat Lahir:</strong>
                     <?php
-                        echo $Tempatlahir
+                    echo $Tempatlahir
                     ?>
                 </p>
                 <p><strong>Tanggal Lahir:</strong>
                     <?php
-                        echo $Tanggallahir
+                    echo $Tanggallahir
                     ?>
                 </p>
                 <p><strong>Hobi:</strong>
                     <?php
-                        echo $Hobi
+                    echo $Hobi
                     ?>
                 </p>
                 <p><strong>Pasangan:</strong>
                     <?php
-                        echo $Pasangan
+                    echo $Pasangan
                     ?>
                 </p>
                 <p><strong>Pekerjaan:</strong>
                     <?php
-                        echo $Pekerjaan
+                    echo $Pekerjaan
                     ?>
                 </p>
                 <p><strong>Nama Orang Tua:</strong>
                     <?php
-                        echo $NamaOrangTua
+                    echo $NamaOrangTua
                     ?>
                 </p>
                 <p><strong>Nama Kakak:</strong>
                     <?php
-                        echo $NamaKakak
+                    echo $NamaKakak
                     ?>
                 </p>
                 <p><strong>Nama Adik:</strong>
                     <?php
-                        echo $NamaAdik
+                    echo $NamaAdik
                     ?>
                 </p>
         </section>
+
+        <section id="IPK">
+            <h2>IPK Saya</h2>
+            <?php
+            $namaMatkul1 = "Logika Informatika";
+            $namaMatkul2 = "Pengantar Teknik Informatika";
+            $namaMatkul3 = "Kalkulus";
+            $namaMatkul4 = "Wawasan Berbudi Luhur";
+            $namaMatkul5 = "Aplikasi Perkantoran";
+
+            $sksMatkul1 = 4;
+            $sksMatkul2 = 4;
+            $sksMatkul3 = 4;
+            $sksMatkul4 = 4;
+            $sksMatkul5 = 4;
+
+            $nilaiHadir1 = 100;
+            $nilaiTugas1 = 90;
+            $nilaiUTS1 = 92;
+            $nilaiUAS1 = 88;
+            $nilaiAkhir1 = (0.1 * $nilaiHadir1) + (0.2 * $nilaiTugas1) + (0.3 * $nilaiUTS1) + (0.4 * $nilaiUAS1);
+
+            $nilaiHadir2 = 95;
+            $nilaiTugas2 = 96;
+            $nilaiUTS2 = 92;
+            $nilaiUAS2 = 88;
+            $nilaiAkhir2 = (0.1 * $nilaiHadir2) + (0.2 * $nilaiTugas2) + (0.3 * $nilaiUTS2) + (0.4 * $nilaiUAS2);
+
+            $nilaiHadir3 = 100;
+            $nilaiTugas3 = 90;
+            $nilaiUTS3 = 92;
+            $nilaiUAS3 = 90;
+            $nilaiAkhir3 = (0.1 * $nilaiHadir3) + (0.2 * $nilaiTugas3) + (0.3 * $nilaiUTS3) + (0.4 * $nilaiUAS3);
+
+            $nilaiHadir4 = 100;
+            $nilaiTugas4 = 95;
+            $nilaiUTS4 = 95;
+            $nilaiUAS4 = 90;
+            $nilaiAkhir4 = (0.1 * $nilaiHadir4) + (0.2 * $nilaiTugas4) + (0.3 * $nilaiUTS4) + (0.4 * $nilaiUAS4);
+
+            $nilaiHadir5 = 100;
+            $nilaiTugas5 = 98;
+            $nilaiUTS5 = 92;
+            $nilaiUAS5 = 95;
+            $nilaiAkhir5 = (0.1 * $nilaiHadir5) + (0.2 * $nilaiTugas5) + (0.3 * $nilaiUTS5) + (0.4 * $nilaiUAS5);
+            // Diatas ini adalah variabel untuk store value dari tiap variabel dan menghitung nilai akhir
+
+            function hitungGrade($hadir, $akhir): string 
+            {
+                if ($hadir <70) return "E"; 
+                elseif ($akhir >= 90) return "A";
+                elseif ($akhir >= 80) return "A-"; 
+                elseif ($akhir >= 75) return "B+";
+                elseif ($akhir >= 70) return "B";
+                elseif ($akhir >= 65) return "B-";
+                elseif ($akhir >= 60) return "C+";
+                elseif ($akhir >= 55) return "C";
+                elseif ($akhir >= 50) return "C-";
+                elseif ($akhir >= 35) return "D";
+                else return "E";
+            }
+
+            
         <section id="contact">
             <h2>Kontak Saya</h2>
             <form action="" method="GET">
