65971ab5c2ced2f50d599f804dc5980e2a469fd7
Abraar2511500015 <2511500015@mahasiswa.atmaluhur.ac.id>
2025-11-26 12:00:14 +0700

    update fungsi.php pertemuan-09

--- pertemuan-09/fungsi.php
+++ pertemuan-09/fungsi.php
@@ -13,3 +13,14 @@ function formattanggal($str)
 {
     return date("d M Y", strtotime($str));
 }
+
+function tampilkanBiodata($conf, $arr)
+{
+    $html = "";
+    foreach ($conf as $k => $v) {
+        $label = $v["label"];
+        $nilai = bersihkan($arr[$k] ?? '');
+        $suffix = $v["suffix"];
+    }
+    return $html
+}
