7a3de05c38d060fc484287e581995e5fdc0f540b
Abraar2511500015 <2511500015@mahasiswa.atmaluhur.ac.id>
2025-11-26 12:03:32 +0700

    update index.php pertemuan-09

--- pertemuan-09/index.php
+++ pertemuan-09/index.php
@@ -123,13 +123,7 @@ endif;
 
     <section id="about">
       <h2>Tentang Saya</h2>
-      <?php foreach ($fieldConfig as $kunci => $metadata): ?>
-        <p>
-          <strong><?= $metadata["label"] ?></strong>
-          <?= htmlspecialchars(string: $biodata[$kunci] ?? "") ?>
-          <?= $metadata["suffix"] ?>
-        </p>
-      <?php endforeach; ?>
+      <?= tampilkanBiodata(conf: $fieldConfig, arr: $biodata) ?>
     </section>
 
     <section id="contact">
