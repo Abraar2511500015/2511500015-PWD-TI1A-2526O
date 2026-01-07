650f9adcf322e1acbc95b67d28f4dc48b0e16660
Abraar2511500015 <2511500015@mahasiswa.atmaluhur.ac.id>
2026-01-07 15:54:37 +0700

    update pertemuan-15

--- pertemuan-15/proses_updatebiodata.php
+++ pertemuan-15/proses_updatebiodata.php
@@ -80,13 +80,20 @@
   */
   if (!empty($errors)) {
     $_SESSION['old'] = [
-      'nama'  => $nama,
-      'email' => $email,
-      'pesan' => $pesan
+        'nim'  => $nim,
+        'nama'  => $nama,
+        'tempat'  => $tempat,       
+        'tanggal'  => $tanggal,
+        'hobi'  => $hobi,
+        'pasangan'  => $pasangan,
+        'pekerjaan'  => $pekerjaan,
+        'ortu'  => $ortu,       
+        'kakak'  => $kakak,
+        'adik'  => $adik,
     ];
 
     $_SESSION['flash_error'] = implode('<br>', $errors);
-    redirect_ke('edit.php?cid='. (int)$cid);
+    redirect_ke('editbiodata.php?cid='. (int)$cid);
   }
 
   /*
@@ -94,35 +101,41 @@
     menyiapkan query UPDATE dengan prepared statement 
     (WAJIB WHERE cid = ?)
   */
-  $stmt = mysqli_prepare($conn, "UPDATE tbl_tamu 
-                                SET cnama = ?, cemail = ?, cpesan = ? 
+  $stmt = mysqli_prepare($conn, "UPDATE tbl_biodata 
+                                SET cnim = ?, cnama = ?, ctempat = ?, ctanggal = ?, chobi = ?, cpasangan = ?, cpekerjaan = ?, cortu = ?, ckakak = ?, cadik = ?
                                 WHERE cid = ?");
   if (!$stmt) {
     #jika gagal prepare, kirim pesan error (tanpa detail sensitif)
     $_SESSION['flash_error'] = 'Terjadi kesalahan sistem (prepare gagal).';
-    redirect_ke('edit.php?cid='. (int)$cid);
+    redirect_ke('editbiodata.php?cid='. (int)$cid);
   }
 
   #bind parameter dan eksekusi (s = string, i = integer)
-  mysqli_stmt_bind_param($stmt, "sssi", $nama, $email, $pesan, $cid);
-
+  mysqli_stmt_bind_param($stmt, "ssssssssssi", $nim, $nama, $tempat, $tanggal, $hobi, $pasangan, $pekerjaan, $ortu, $kakak, $adik, $cid);
   if (mysqli_stmt_execute($stmt)) { #jika berhasil, kosongkan old value
     unset($_SESSION['old']);
     /*
       Redirect balik ke read.php dan tampilkan info sukses.
     */
     $_SESSION['flash_sukses'] = 'Terima kasih, data Anda sudah diperbaharui.';
-    redirect_ke('read.php'); #pola PRG: kembali ke data dan exit()
+    redirect_ke('read_biodata.php'); #pola PRG: kembali ke data dan exit()
   } else { #jika gagal, simpan kembali old value dan tampilkan error umum
     $_SESSION['old'] = [
-      'nama'  => $nama,
-      'email' => $email,
-      'pesan' => $pesan,
+        'nim'  => $nim,
+        'nama'  => $nama,
+        'tempat'  => $tempat,
+        'tanggal'  => $tanggal,
+        'hobi'  => $hobi,
+        'pasangan'  => $pasangan,
+        'pekerjaan'  => $pekerjaan,
+        'ortu'  => $ortu,
+        'kakak'  => $kakak,
+        'adik'  => $adik,
     ];
     $_SESSION['flash_error'] = 'Data gagal diperbaharui. Silakan coba lagi.';
-    redirect_ke('edit.php?cid='. (int)$cid);
+    redirect_ke('editbiodata.php?cid='. (int)$cid);
   }
   #tutup statement
   mysqli_stmt_close($stmt);
 
-  redirect_ke('edit.php?cid='. (int)$cid);
+  redirect_ke('editbiodata.php?cid='. (int)$cid);
